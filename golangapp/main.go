6b9684d9cb52b3784a7531d12a2e88ae2a92ba7b
Angad Thandi <angadthandi8@gmail.com>
2018-10-04 20:11:32 -0600

    Add MongoDb as service & connect to the mongodb service

--- golangapp/main.go
+++ golangapp/main.go
@@ -6,24 +6,45 @@ import (
 	"net/http"
 
 	"github.com/angadthandi/golangmongoapp/golangapp/test"
+	mgo "gopkg.in/mgo.v2"
 )
 
-var ServerPort = ":9002"
+var (
+	ServerPort           = ":9002"          // http server port
+	mongodatabaseservice = "mongodbservice" // mongodb service name
+)
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test Page! %s", test.Echo())
+// test handler
+func testHandler(
+	w http.ResponseWriter,
+	r *http.Request,
+	db *mgo.Session,
+) {
+	fmt.Fprintf(w, "Test Page! %s", test.Echo(db))
 }
 
+// handler for home
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page! %s", r.URL.Path[1:])
 }
 
 func main() {
+	// connect to database
+	dbSession, err := mgo.Dial(mongodatabaseservice)
+	if err != nil {
+		log.Fatalf("mongodb connection error : %v", err)
+	}
+
+	defer dbSession.Close()
+
+	dbSession.SetMode(mgo.Monotonic, true)
 
 	fmt.Printf("Listening on Port: %v", ServerPort)
 
 	// start http web server
-	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		testHandler(w, r, dbSession)
+	})
 	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(ServerPort, nil))
 }
--- golangapp/test/test.go
+++ golangapp/test/test.go
@@ -1,5 +1,23 @@
 package test
 
-func Echo() string {
-	return "Echo from test pkg!"
+import (
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func Echo(db *mgo.Session) string {
+	c := db.DB("testdb").C("people")
+	_ = c.Insert(
+		&Person{"Alex", "+55 89 9556 9111"},
+		&Person{"John", "+55 98 8402 3256"})
+
+	result := Person{}
+	_ = c.Find(bson.M{"name": "Alex"}).One(&result)
+
+	return result.Phone
 }
