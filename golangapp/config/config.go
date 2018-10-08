c4b1de4c5258b81133a7405a39d1eba01633740a
Angad Thandi <angadthandi8@gmail.com>
2018-10-07 23:11:47 -0600

    MongoDB Auth

---
+++ golangapp/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	ServerPort = ":9002" // http server port
+
+	// MongoDB
+	MongoDBPort        = ":27017"
+	MongoDBServiceName = "mongodbservice"
+	MongoDBUsername    = "root"
+	MongoDBPassword    = "password"
+)
--- golangapp/main.go
+++ golangapp/main.go
@@ -5,13 +5,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/angadthandi/golangmongoapp/golangapp/config"
 	"github.com/angadthandi/golangmongoapp/golangapp/test"
 	mgo "gopkg.in/mgo.v2"
-)
-
-var (
-	ServerPort           = ":9002"          // http server port
-	mongodatabaseservice = "mongodbservice" // mongodb service name
 )
 
 // test handler
@@ -30,7 +26,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// connect to database
-	dbSession, err := mgo.Dial(mongodatabaseservice)
+	dbSession, err := mgo.Dial(
+		"mongodb://" +
+			config.MongoDBUsername + ":" +
+			config.MongoDBPassword + "@" +
+			config.MongoDBServiceName +
+			config.MongoDBPort)
 	if err != nil {
 		log.Fatalf("mongodb connection error : %v", err)
 	}
@@ -39,12 +40,12 @@ func main() {
 
 	dbSession.SetMode(mgo.Monotonic, true)
 
-	fmt.Printf("Listening on Port: %v", ServerPort)
+	fmt.Printf("Listening on Port: %v", config.ServerPort)
 
 	// start http web server
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		testHandler(w, r, dbSession)
 	})
 	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(ServerPort, nil))
+	log.Fatal(http.ListenAndServe(config.ServerPort, nil))
 }
