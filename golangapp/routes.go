ad3ea9329b9ba88fccbc45f1249c2c616981e5fa
Angad Thandi <angadthandi8@gmail.com>
2018-10-27 17:10:37 -0600

    Use gorilla mux router

--- golangapp/routes.go
+++ golangapp/routes.go
@@ -4,22 +4,27 @@ import (
 	"net/http"
 
 	"github.com/angadthandi/golangmongoapp/golangapp/test"
+	"github.com/gorilla/mux"
 	mgo "gopkg.in/mgo.v2"
 )
 
 // configure API Routes
 func configureRoutes(dbSession *mgo.Session) {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
 		api(w, r, dbSession, MessagingClient)
 	})
 
 	// Test Routes --------------------------------
 
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		test.TestHandler(w, r, dbSession)
 	})
-	http.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request) {
 		test.SendMQ(w, r, MessagingClient)
 	})
+
+	http.Handle("/", r)
 }
